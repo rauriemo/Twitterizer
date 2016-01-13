2fd8b6e494ea24eedd25c482a0e9757cec186f33
Rafael Auriemo <rafael.auriemo@gmail.com>
2016-01-13 13:13:45 -0800

    Initial Commit

---
+++ TwitterizerUITests/TwitterizerUITests.m
@@ -0,0 +1,40 @@
+//
+//  TwitterizerUITests.m
+//  TwitterizerUITests
+//
+//  Created by Rafael Auriemo on 1/13/16.
+//  Copyright © 2016 Rafael Auriemo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TwitterizerUITests : XCTestCase
+
+@end
+
+@implementation TwitterizerUITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
